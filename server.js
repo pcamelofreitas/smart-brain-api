517814f4c9a66fbe46466559a314243dd573f31e
pcamelofreitas <39499125+pcamelofreitas@users.noreply.github.com>
2022-02-11 12:38:33 -0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const bcrypt = require('bcrypt');
+const cors = require("cors");
+const knex = require('knex');
+
+const register =require('./controllers/register');
+const signin = require("./controllers/signin");
+const profile = require('./controllers/profile');
+const image = require('./controllers/image');
+
+//define db from knex
+const db = knex({
+    client: 'pg',
+    connection: {
+      host : '127.0.0.1',
+      port : 5432,
+      user : 'postgres',
+      password : '123456',
+      database : 'smart-brain'
+    }
+});
+
+//express
+const app = express();
+app.use(express.json());
+app.use(cors())
+
+
+//index
+app.get('/',(req,res)=>{res.send(db.users)})
+
+//logar
+app.post('/signin', (req,res)=>{signin.handleSignin(req,res,db,bcrypt)})
+
+//registro-adicionar na db
+app.post('/register', (req,res)=>{register.handleRegister(req,res,db,bcrypt)})
+
+// requisitar perfil id pra açoes futuras
+app.get('/profile/:id',(req,res) => {profile.handleProfileGet(req,res,db)})
+
+//quando enviar imagem aumentar a contagem
+app.put('/image',(req,res)=>{image.handleImage(req,res,db)})
+app.post('/imageurl',(req,res)=>{image.handleApiCall(req,res)})
+
+app.listen(3000,()=>{
+console.log('app is running on port 3000')
+})
